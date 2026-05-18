f56f237af66bcd436333a519d3592f08326a9043
uthman adeyemi <uthmanadeyemi001@gmail.com>
2026-05-18 21:22:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const ageCheck =() => {
+    let age = 18
+    const ageCheck = () => {
+        const age = +prompt("Enter your age:");
+        if (age >= 18) {
+            alert("YOU ARE ELIGIBLE TO DRIVE");
+        } else {
+            alert("YOU ARE NOT ELIGIBLE TO DRIVE");
+        }
+    };
+}
